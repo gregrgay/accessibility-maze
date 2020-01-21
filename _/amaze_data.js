ac61497a0a0c9a3b70c339d325d749b92fae3ff4
Greg Gay <gregrgay@gmail.com>
2020-01-21 16:15:31 -0500

    updated game language

--- _/amaze_data.js
+++ _/amaze_data.js
@@ -1,19 +1,19 @@
 {
     "gameinfo": {
 	    "about": "<h1>About</h1><div class='message'><p>The Accessibility Maze was developed by the Digital Education Strategies team at The Chang School of Continuing Education, at Ryerson University. Funding is provided by The Chang School, and the Government of Ontario.</p><p>The purpose of the game is to introduce those new to web accessibility, to potential barriers on the Web that may prevent some people from accessing content. You will experience the challenges of navigating the Web that some people with disabilities encounter.</p></div>",
-	    "howto": "<h1>How to Play</h1><div class='message'><p>The goal of the Accessibility Maze is to navigate through the maze, solving puzzles to move between levels, and collecting gems to be exchanged for a reward after completing the game.</p><p>Once in the maze, use your keyboard's arrow keys to move up and down, or left and right, through the squares in the maze.</p><p>Press the arrow keys while next to an object, to activate that object.</p><p>Use the Escape (ESC) key to dismiss any dialog boxes that may open after reading their content.</p><p>Your position in the game is maintained when returning to the menu, so you can continue where you left off in the game by clicking Resume Game. Choosing New Game will start the game over from the beginning.\n</p></div>",
+	    "howto": "<h1>How to Play</h1><div class='message'><p>The goal of the Accessibility Maze is to navigate through the maze, solving puzzles to move between levels, and collecting gems to be exchanged for a reward after completing the game.</p><p>Once in the maze, use your keyboard's arrow keys to move up and down, or left and right, through the squares in the maze.</p><p>Press the arrow keys while next to an object, to bump up against the object and activate it.</p><p>Use the Escape (ESC) key to dismiss any dialog boxes, puzzles, or diary pages that may open, after reading their content.</p><p>Your position in the game is maintained when returning to the menu, so you can continue where you left off in the game by clicking Resume Saved. Choosing New Game will start the game over from the beginning.\n</p></div>",
 	    "contacts": "<h1>Contact Us</h1><div class='message'><p>Questions or comments about the game can be directed to <a href='mailto:dehelp@ryerson.ca'>dehelp@ryerson.ca</a>.</p></div>",
-        "accessibility": "<h1>Accessibility</h1><div class='message'><p>Every effort has been made to make the Accessibility Maze accessible to screen readers.</p><p>The background music can be disabled using the Audio button at the top of the maze, once it opens.</p><p>Next to the audio button, is another button that returns the player to the menu on the opening screen of the game.</p><p>Listen for shortcuts during game play, announced by screen reader.</p></div>",
-	    "terms": "<h1>Terms of Use</h1><div><p>Unless otherwise noted this resource is available for public use under <a href=\"https://creativecommons.org/licenses/by-nc/4.0/\" target=\"postop\">Creative Commons Attribution-NonCommercial 4.0 International (CC BY-NC 4.0)</a>.</p><p>Under the terms of Creative Commons license, you are free to share and adapt this resource under the following terms: If you share the content from this resource, you must identify the creators of the resource (Centennial College, George Brown College, and Ryerson University) in a reasonable manner. Also, you may not use the material for commercial purposes.</p></div>"
+        "accessibility": "<h1>Accessibility</h1><div class='message'><p>Every effort has been made to make the Accessibility Maze accessible to screen readers.</p><p>The background music can be disabled using the Audio button at the top of the maze, once it opens.</p><p>Next to the audio button, is another button that returns the player to the menu on the opening screen of the game.</p><p>Listen for shortcuts during game play, announced by screen readers.</p></div>",
+	    "terms": "<h1>Terms of Use</h1><div><p>Unless otherwise noted this resource is available for public use under <a href=\"https://creativecommons.org/licenses/by-sa/4.0/\" target=\"postop\">Creative Commons Attribution-ShareAlike 4.0 International (CC BY-SA 4.0)</a>.</p><p>Under the terms of Creative Commons license, you are free to share and adapt this resource under the following terms: If you share the content from this resource, you must identify the creators of the resource (Ryerson University) in a reasonable manner. </p></div>"
     },
     "intro": [
         {
             "image": "_/img/intro/intro_01.png",
-            "content": "<p>Professor X. and I were doing a research on the mysterious Accessibility Master. Rumors had it that he had discovered a great secret that would make the entire world accessible.</p><p>Unfortunately, the Master's \"discovery\" was never published and he himself disappeared without a trace. Nobody has heard from him since.</p>"
+            "content": "<p>Professor X. and I were doing research on the mysterious Accessibility Master. Rumors had it that he had found a great secret that would make the entire world accessible.</p><p>Unfortunately, the Master's work was never published and he himself disappeared without a trace. Nobody has heard from him since.</p>"
         },
         {
             "image": "_/img/intro/intro_02.png",
-            "content": "<p>Last night, Professor X. called to tell me that he had found the secret of the Accessibility Master's disappearance. I had some other commitments, so I promised to see him next morning.</p>"
+            "content": "<p>Last night, Professor X called to tell me that he had found the secret of the Accessibility Master's disappearance. I had some other commitments that evening, so I promised to see him next morning.</p>"
         },
         {
             "image": "_/img/intro/intro_03.png",
@@ -25,7 +25,7 @@
         },
         {
             "image": "_/img/intro/intro_05.png",
-            "content": "<p>The Professor X. was not there yet. Suddenly, I saw a note on the floor. I immediately recognized the Professor's writing.</p>"
+            "content": "<p>The Professor X was not there yet. Suddenly, I saw a note on the floor. I immediately recognized the Professor's writing.</p>"
         },
         {
             "image": "_/img/intro/intro_06.png",
@@ -119,7 +119,7 @@
             }
         ],
         "inventory": [],
-        "lesson": "<h1>All visual content must have text alternatives</h1><p>The cat's collar on that picture clearly displayed his name. However, when the picture got accidentally splattered with ink, the only way to get the password was to read the text at the back.</p><p>On the web it is very important to provide text alternatives to any visual information. Adding ALT attribute to images, or captions to videos will ensure that no one is left out.</p>"
+        "lesson": "<h1>LESSON 1: All visual content must have a text alternative.</h1><p>When creating content for the Web, one of the most important accessibility features that can be added is text descriptions for meaningful visual content. This ensures that people who are blind get equivalent information to those who can see images. In this puzzle the cat's name on the back of the photo, provides a text alternative for the name that cannot be seen in the image .</p><p>Text alternatives also make it possible for search engines to index images to make them searchable. It makes the same information available for those with images turned off, or perhaps using older technology, or with limited data plans. Learn more about providing text alternatives when you reach the end of the game.</p>"
     },
     {
             "floorplan": [
@@ -208,7 +208,7 @@
                 }
             ],
             "inventory": [],
-            "lesson": "<h1>Ensure all controls and buttons are properly labelled</h1><p>You just learned the importance of properly labelling interactive controls, like buttons. Please remember that simply placing some text or images next to them is not enough. People who use screen readers might not perceive the connection.</p><p>To make those controls accessible, each one of them must be properly associated with a corresponding label. Since engraved metal plates don't work well on the Web, you'll need to learn special techniques to make those connections.</p>"
+            "lesson": "<h1>LESSON 2: Ensure all buttons and form fields are properly labelled</h1><p>You just experienced what happens when buttons are not labelled properly. Remember that simply placing some text or images next to buttons or form fields is not enough — people who use screen readers might not perceive the connection.</p><p>To make buttons and form fields accessible, each one of them must be properly associated with a label. Complete the game to receive tips on how to do this!</p>"
         },
         {
             "floorplan": [
@@ -229,7 +229,7 @@
                     "row": 3,
                     "col": 5,
                     "data": {
-	                    "content": "<div class='columns'><p><u>Wednesday, May 8</u></p><p>After pestering Tom for two months, I finally persuaded him to create a self-closing mechanism for my cabinet doors.</p><p>Amazing craftsmanship as usual! However, the mechanism is too powerful. It shuts the door before I can take out or put anything in. Last night I nearly lost my fingers!</p><p>I don't want to ask Tom to look at it again. He's a nice guy, but it will likely take him another two months to fix it.</p><p>Instead, I came up with this ingenious solution: I'm going to use a freeze spray to slow down the closing mechanism!</p></div>"
+	                    "content": "<div class='columns'><p><u>Wednesday, May 8</u></p><p>After pestering Tom for two months, I finally persuaded him to create a self-closing mechanism for the door to the next room.</p><p>Amazing craftsmanship as usual! However, the mechanism is too powerful. It shuts the door before I can get to it. Last night I nearly lost my fingers!</p><p>I don't want to ask Tom to look at it again. He's a nice guy, but it will likely take him another two months to fix it.</p><p>Instead, I came up with this ingenious solution: I'm going to use a freeze spray to slow down the closing mechanism!</p></div>"
                     }
 	            },
                 {
@@ -316,7 +316,7 @@
                 }
             ],
             "inventory": [],
-            "lesson": "<h1>Fast moving or self-updating content should be easily paused, or slowed down</h1><p>Not everyone can interact with websites or devices at the same speed. Some people with cognitive disabilities, or mobility impairments might require more time to complete a task.</p><p>It is important to always provide a way to slow down, or completely stop any moving or self-updating content on the Web. This would include auto-scrolling text, automated slide shows, audio and video content, etc.</p>"
+            "lesson": "<h1>LESSON 3: Moving or timed content should be easily paused, or slowed down</h1><p>Not everyone can interact with websites or devices at the same speed -- some users require more time to complete a task.</p><p>It is important to always provide a way to completely stop or slow down any moving or timed content on the Web. Complete the game to learn more. </p>"
         },
         {
             "floorplan": [
@@ -337,7 +337,7 @@
                     "row": 1,
                     "col": 3,
                     "data": {
-	                    "content": "<div class='columns'><p><u>Tuesday, June 4</u></p><p>This morning I tired to open Tom's particle skimmer holding a molten core specimen with my both hands. After 10 minutes of futile attempts I was ready to stuff that piece of core up his... </p><p class='column_break_after'>Well, Tom is a nice guy and a brilliant engineer, but his attitude sometimes drives me crazy! How many times I have to tell him, that all those machines and devices that he creates for me have to be easily operated not just by hands, but also by something else, like a foot, an elbow... a long stick, for pity's sake! </p><p><u>Wednesday, June 5</u></p><p>I think I need to teach Tom a lesson and I might just know how! For the last couple of months I have been experimenting with a new insulation material. It is extremely strong and can only be pierced by something sharp and pointy, like an arrow.</p><p>I filled the entire room next to the exit with air balloons made of that thing. I'd like to see him try and make his way to the exit using his bare hands!</p></div>"
+	                    "content": "<div class='columns'><p><u>Tuesday, June 4</u></p><p>This morning I tired to open Tom's particle skimmer holding a molten core specimen with my two hands. After 10 minutes of futile attempts I was ready to stuff that piece of core up his... </p><p class='column_break_after'>Well, Tom is a brilliant engineer, but his attitude sometimes drives me crazy! How many times do I have to tell him, that all those machines and devices that he creates for me have to be easily operated not just by hands, but also by something else, like a foot, an elbow... a long stick, for pity's sake! </p><p><u>Wednesday, June 5</u></p><p>I think I need to teach Tom a lesson and I might just know how! For the last couple of months I have been experimenting with a new insulation material. It is extremely strong and can only be pierced by something sharp and pointy, like an arrow.</p><p>I filled the entire room next to the exit with air balloons made of the material. Now I'd like to see him try and make his way to the exit using his bare hands!</p></div>"
                     }
 	            },
                 {
@@ -411,7 +411,7 @@
                 }
             ],
             "inventory": [],
-            "lesson": "<h1>Do not rely on one input device only.</h1><p>All web content must be operable regardless of the input device used. That means that if users can open a website navigation menu or select a date from a popup calendar with a mouse, they should be able to do exactly the same with a keyboard, joystick, or any other input device.</p>"
+            "lesson": "<h1>Lesson 4: Interactive features must operate with a mouse and a keyboard.</h1><p>Many people are not able to use a mouse, perhaps because they cannot see the mouse pointer, or perhaps they do not have a steady enough hand to target clickable features effectively, among other reasons. </p><p>You may have experienced a moment of frustration yourself trying to access the final room in this puzzle when your keyboard stopped working. This frustration is common for people who rely on a keyboard to navigate the Web. Learn more about \"device independence\" at the end of the game.</p>"
         },
         {
             "floorplan": [
