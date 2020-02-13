860a5388409423ebf9040c3966aa818ccd53e480
Igor Karasyov <ikarasyo@ryerson.ca>
2020-02-13 16:59:37 -0500

    - updated instructions; - updated tab key handlers.

--- _/amaze.js
+++ _/amaze.js
@@ -43,8 +43,6 @@ app.config(['$routeProvider', '$locationProvider',
 		$rootScope.isMobile = /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(browser) ||
 			                  /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test( browser.substr(0,4) );
 
-		console.log($rootScope.isMobile);
-
 		$rootScope = _.extend($rootScope, {
 			gameinfo: [],
 			intro: [],
@@ -112,9 +110,9 @@ app.config(['$routeProvider', '$locationProvider',
 				{id:"win", src:"_/snd/fanfare.mp3"}
 			];
 
-			_.each($rootScope.gameinfo.images, function(el, ind) {
+			/*_.each($rootScope.gameinfo.images, function(el, ind) {
 				assets.push({id: ind, src: el});
-			});
+			});*/
 			_.each($rootScope.intro, function(el, ind) {
 				assets.push({id: "intro_" + (++ind), src: el.image});
 			});
@@ -160,8 +158,6 @@ app.config(['$routeProvider', '$locationProvider',
 		$rootScope.toggleAmbientSound = function($event) {
 			var instance = "ambientSound";
 			$event.preventDefault();
-			//$event.stopPropagation();
-
 			if ($event.type == 'click' || $event.keyCode == 32 || $event.keyCode == 13) {
 				if ($rootScope.game.settings.music) {
 					$rootScope.stopSound(instance);
@@ -172,10 +168,8 @@ app.config(['$routeProvider', '$locationProvider',
 				}
 				$rootScope.game.settings.music = !$rootScope.game.settings.music;
 				$rootScope.saveState();
-			} else if($event.keyCode == 9) {
-				$rootScope.moveFocusOnTab($event);
 			}
-		}
+		};
 
 		$rootScope.toggleDialogFocus = function(show) {
 			var $focusable, $overlay;
@@ -218,30 +212,25 @@ app.config(['$routeProvider', '$locationProvider',
 
 		$rootScope.openMenu = function($event) {
 			$event.preventDefault();
-			//$event.stopPropagation();
 			if ($event.type == 'click' || $event.keyCode == 32 || $event.keyCode == 13) {
 				$location.path("/menu");
 				window.gtag("event", "Menu", { event_category: "User Experience", event_label: "Exit to Menu"});
-			} else if($event.keyCode == 9) {
-				$rootScope.moveFocusOnTab($event);
 			}
 		};
 
 		$rootScope.moveFocusOnTab = function ($event) {
 			var $focusable, ind, next;
-
 			$event.preventDefault();
-			//$event.stopPropagation();
-
-			$focusable = $("button, [tabindex]").filter(":visible");
-			ind = $focusable.index($($event.currentTarget));
-
-			if($event.shiftKey) {
-				next = ind == 0 ? $focusable.length - 1 : 0;
-			} else {
-				next = ind > $focusable.length - 1 ? 0 : ++ind;
+			if ($event.keyCode == 9) {
+				$focusable = $("button, [tabindex]").filter(":visible");
+				ind = $focusable.index( $(":focus") );
+				if ($event.shiftKey) {
+					next = (ind == 0) ? $focusable.length - 1 : --ind;
+				} else {
+					next = (ind > $focusable.length - 2) ? 0 : ++ind;
+				}
+				$focusable.eq(next).focus();
 			}
-			$focusable.eq(next).focus();
 		};
 
 		$rootScope.actionLog = "";
@@ -376,8 +365,9 @@ app.controller('levelCtrl', ['$rootScope', '$scope', '$location', '$storage', '$
 			if ($rootScope.game.settings.music) {
 				$rootScope.playSound($rootScope.ambientSoundName, {loop: -1, volume: .1}, "ambientSound");
 			}
+
 			if ($rootScope.game.firstTime) {
-				$scope.message = "<h1>How to Play</h1><ul><li>Use KEYBOARD to play the game:<ul><li>Arrow keys &mdash; move around</li><li>Enter key &mdash; press buttons</li><li>Esc key &mdash; close popup boxes (including this one)</li></ul></li><li>Bump into things to interact</li><li>Pay attention to status messages below the map</li></ul>"
+				$scope.message = $rootScope.gameinfo.howto;
 				$rootScope.toggleDialogFocus(true);
 				$rootScope.game.firstTime = false;
 				$rootScope.saveState();
@@ -487,6 +477,9 @@ app.controller('levelCtrl', ['$rootScope', '$scope', '$location', '$storage', '$
 						break;
 					case 40: // bottom
 						takeAction(event, ++row, col, "down");
+						break;
+					case 9:
+						$rootScope.moveFocusOnTab(event);
 						break;
 					default:
 						//console.log(event.keyCode)
@@ -768,8 +761,13 @@ app.controller('puzzle1Ctrl', ['$rootScope', '$scope', '$location', '$timeout',
 		};
 
 		$scope.puzzle1KeyDownHandler = function(event) {
-			if (event.keyCode == 27) {
-				$location.path('/level');
+			switch (event.keyCode) {
+				case 27:
+					$location.path('/level');
+					break;
+				case 9:
+					$rootScope.moveFocusOnTab(event);
+					break;
 			}
 		};
 		$scope.toggleButton = function($event, $index) {
@@ -852,9 +850,14 @@ app.controller('puzzle2Ctrl', ['$rootScope', '$scope', '$location', '$timeout',
 		}
 		$scope.showPhoto = nextTile.ready;
 		$scope.hint = nextTile.data.hint;
-			$scope.puzzleKeyDownHandler = function(event) {
-			if (event.keyCode == 27) {
-				$location.path('/level');
+		$scope.puzzleKeyDownHandler = function(event) {
+			switch (event.keyCode) {
+				case 27:
+					$location.path('/level');
+					break;
+				case 9:
+					$rootScope.moveFocusOnTab(event);
+					break;
 			}
 		};
 		$scope.enterCode = function(event, letter) {
